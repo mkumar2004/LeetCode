8fc4839f9ca9cd1e374f2aac090fa07679ff360f
Manojkumar <mkumarrrr22@gmail.com>
2025-11-24 21:56:34 +0530

    Day 1

---
+++ java/SumOfTwoNum.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+class SumOfTwoNum {
+
+    public static void main(String[] args) {
+
+        int[] a = {2, 7, 11, 15};
+        int[] a2 = {3, 2, 4};
+        int[] a3 = {3, 3};
+
+        int t1 = 9;
+        int t2 = 6;
+
+      
+        System.out.println(Arrays.toString(twoSum(a, t1)));
+        System.out.println(Arrays.toString(twoSum(a2, t2)));
+        System.out.println(Arrays.toString(twoSum(a3, t2)));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+
+            }
+        }
+
+        return new int[]{-1, -1}; 
+    }
+}
